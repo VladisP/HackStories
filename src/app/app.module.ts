203491a3ec6565a381a22c238a8243d44a5d174b
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-06 00:04:30 +0300

    [ADD] header & toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [AppComponent],
@@ -16,6 +17,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
         MatButtonModule,
         MatIconModule,
         MatSnackBarModule,
+        MatToolbarModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
