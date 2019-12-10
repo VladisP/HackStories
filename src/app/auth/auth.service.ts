e83f416a2ea4bbc1ac2374edf024a1b11dc3467e
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-10 20:07:54 +0300

    [ADD] primitive auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    private loggedIn$ = new BehaviorSubject<boolean>(false);
+
+    get isLoggedIn(): boolean {
+        return this.loggedIn$.getValue();
+    }
+
+    get isLoggedIn$(): Observable<boolean> {
+        return this.loggedIn$.asObservable();
+    }
+
+    login() {
+        this.loggedIn$.next(true);
+    }
+
+    logout() {
+        this.loggedIn$.next(false);
+    }
+}
