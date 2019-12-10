5fc15d7867f486c1212dd31761acb7eee361cb7e
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-10 13:13:37 +0300

    [ADD] route from story-list to hn-user profile

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -15,4 +15,24 @@ export class StoryComponent {
     showNotification() {
         this.snackBar.open('Статья добавлена в избранное', 'Закрыть', {duration: 2000});
     }
+
+    get title(): string {
+        return (<IStory>this.story).title;
+    }
+
+    get author(): string {
+        return (<IStory>this.story).author;
+    }
+
+    get points(): number {
+        return (<IStory>this.story).points;
+    }
+
+    get datetime(): Date {
+        return (<IStory>this.story).time;
+    }
+
+    get url(): string {
+        return (<IStory>this.story).url;
+    }
 }
