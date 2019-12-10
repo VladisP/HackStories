f277edd1d161e8c95bb7f8522e899bf1e768c812
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-11 00:56:03 +0300

    [ADD] routes from header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
     selector: 'tfs-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-    constructor() {}
+export class HeaderComponent implements OnDestroy {
+    isLoggedIn = this.authService.isLoggedIn;
 
-    ngOnInit() {}
+    private authSubscription = this.authService.isLoggedIn$.subscribe(
+        newState => (this.isLoggedIn = newState),
+    );
+
+    constructor(private authService: AuthService) {}
+
+    ngOnDestroy() {
+        this.authSubscription.unsubscribe();
+    }
 }
