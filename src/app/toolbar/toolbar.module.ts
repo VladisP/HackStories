3210cc376cc2d72e6716525d63d1499aa0c860c0
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-12-12 19:50:25 +0300

    [UPD] profile icon

--- src/app/toolbar/toolbar.module.ts
+++ src/app/toolbar/toolbar.module.ts
@@ -3,11 +3,18 @@ import {CommonModule} from '@angular/common';
 import {ToolbarComponent} from './toolbar.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import {RouterModule} from '@angular/router';
 
 @NgModule({
     declarations: [ToolbarComponent],
     exports: [ToolbarComponent],
-    imports: [CommonModule, MatIconModule, MatToolbarModule, RouterModule.forChild([])],
+    imports: [
+        CommonModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatButtonModule,
+        RouterModule.forChild([]),
+    ],
 })
 export class ToolbarModule {}
